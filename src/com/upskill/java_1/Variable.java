b2ac0a6d31d16cc1141c418ea6b847241625c69e
ZahidulIslam1979 <Zahidoshin1979@gmail.com>
2022-09-25 13:16:13 -0400

    2nd

--- src/com/upskill/java_1/Variable.java
+++ src/com/upskill/java_1/Variable.java
@@ -1,10 +1,31 @@
 package com.upskill.java_1;
 
 public class Variable {
+	
+	// Variables in Java
+	
+	public String country = "USA";
+	
+	public int a = 2;
+	
+	public static String region = "America";
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+	 int b =2;
+				String city = "Nyc";  	
+				System.out.println(region);// Local variable -  variables declared in methods
+				
+				Variable obj = new Variable();
+				System.out.println(obj.a);
+				obj.method("");
+			}
+			
+		    public void method(String county) {		    	// Method parameter - variables used as method parameter
+		    	String myCounty = county;
+		 }
+		
 	}
 
-}
+
